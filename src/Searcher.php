e614546c80c719da30c98857103a6a1add7cbf2f
Spaceman1994 <farzin1509@gmail.com>
2023-08-26 16:58:10 +0330

    Do requirement 1

---
+++ src/Searcher.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Exercise4;
+
+class Searcher
+{
+    public function search(string $input): string
+    {
+        return '';
+    }
+}
---
+++ tests/Unit/SearchTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Exercise4\Tests\Unit;
+
+require_once 'src/Searcher.php';
+
+use Exercise4\Searcher;
+use PHPUnit\Framework\TestCase;
+
+class SearchTest extends TestCase
+{
+    public function testShouldReturnEmptyWhenInputLessThanTwoCharacters(): void
+    {
+        $searcher = new Searcher();
+
+        $result = $searcher->search('a');
+
+        $this->assertSame( '', $result);
+    }
+}
